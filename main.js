75c142dd303e42194b47a2e484e51e3a51d0a2ff
MarcAS2022 <109641139+MarcAS2022@users.noreply.github.com>
2024-02-16 15:18:18 +0000

    chan

--- main.js
+++ main.js
@@ -2,5 +2,5 @@
 
 document.getElementById('funButton').addEventListener('click', function() {
     const funText = document.getElementById('funText');
-    funText.innerHTML = '🎉 Congratulations on taking your first step into web development with CS 140! 🎉';
+    funText.innerHTML = 'You can now redeem the hidden code -tulips 🎉';
 });
